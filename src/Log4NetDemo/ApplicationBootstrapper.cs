7df7d7f9239b95e4eac74e7a85b82aaa1098bc2c
CodeFuller <CodeFuller@gmail.com>
2021-08-01 16:12:20 +0300

    Add skeleton of console application

---
+++ src/Log4NetDemo/ApplicationBootstrapper.cs
@@ -0,0 +1,24 @@
+﻿using CodeFuller.Library.Bootstrap;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Log4NetDemo
+{
+	internal class ApplicationBootstrapper : BasicApplicationBootstrapper<IApplicationLogic>
+	{
+		protected override void RegisterServices(IServiceCollection services, IConfiguration configuration)
+		{
+			services.AddSingleton<IApplicationLogic, ApplicationLogic>();
+		}
+
+		protected override void BootstrapConfiguration(IConfigurationBuilder configurationBuilder, string[] commandLineArgs)
+		{
+			// This application does not use configuration.
+		}
+
+		protected override void BootstrapLogging(ILoggerFactory loggerFactory, IConfiguration configuration)
+		{
+		}
+	}
+}
---
+++ src/Log4NetDemo/ApplicationLogic.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using CodeFuller.Library.Bootstrap;
+using Microsoft.Extensions.Logging;
+
+namespace Log4NetDemo
+{
+	internal class ApplicationLogic : IApplicationLogic
+	{
+		private readonly ILogger<ApplicationLogic> logger;
+
+		public ApplicationLogic(ILogger<ApplicationLogic> logger)
+		{
+			this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
+		}
+
+		public Task<int> Run(string[] args, CancellationToken cancellationToken)
+		{
+			logger.LogInformation("Starting application ...");
+
+			logger.LogInformation("Exiting application ...");
+
+			return Task.FromResult(0);
+		}
+	}
+}
